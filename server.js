e26b1844bcf69073ebf6c28bc658fb98fbc0369b
Nicolas Dutil <ndutil79@gmail.com>
2014-07-15 00:07:06 -0400

    test

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ app.use(express.logger());
 
 var title = "NuAyre Eliminateurs d'odeurs Produits EcoLogo UL 2796";
 
-var oneDay = 86400000;
+var oneday = 86400000;
 
 app.use(express.compress());
-app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+app.use(express.static(__dirname + '/public', { maxAge: oneday }));
 
 app.get('/', function(req, res){
     res.sendfile('index.html');
